397eae64fbb8c6b7882a6d24fe85492f20ec8f6a
Christian Fobel <christian@fobel.net>
2019-10-16 14:24:26 -0400

    fix(notebooks/rpc_host): add RPC host
    Prior to this commit, the `rpc_host.py` file was not included in the
    repo. This caused an import error in the `MicroPython RPC - pump UI.py`
    notebook.
    
    In this commit, add the `rpc_host.py` module containing the RPC remote
    class to interface with the ESP32 event loop.

---
+++ notebooks/rpc_host.py
@@ -0,0 +1,114 @@
+'''
+.. versionadded:: X.X.X
+'''
+import asyncio
+import json
+import os
+import pathlib
+
+
+class RemoteBase:
+    def __init__(self, device):
+        self.device = device
+
+    def _base_call(self, base_message, command, *args, **kwargs):
+        raise NotImplementedError
+
+    def call(self, command, *args, **kwargs):
+        return self._base_call({}, command, *args, **kwargs)
+
+    def await_(self, command, *args, **kwargs):
+        return self._base_call({'async': True}, command, *args, **kwargs)
+
+    def create_task(self, command, *args, **kwargs):
+        return self._base_call({'async': 'task'}, command, *args, **kwargs)
+
+
+class Remote(RemoteBase):
+    def _base_call(self, base_message, command, *args, **kwargs):
+        message = base_message.copy()
+        message.update({'command': command, 'args': args,
+                        'kwargs': kwargs})
+        self.device.write(json.dumps(message).encode('utf8') + b'\r\n')
+        response = json.loads(self.device.readline())
+        if 'error' in response:
+            raise RuntimeError('Error: `%s`' % response['error'])
+        return response['result']
+
+
+class AsyncRemote(RemoteBase):
+    def __init__(self, *args, **kwargs):
+        self.lock = asyncio.Lock()
+        super().__init__(*args, **kwargs)
+
+    async def _base_call(self, base_message, command, *args, **kwargs):
+        message = base_message.copy()
+        message.update({'command': command, 'args': args,
+                        'kwargs': kwargs})
+        with await self.lock:
+            await self.device.write(json.dumps(message).encode('utf8') +
+                                    b'\r\n')
+            response = json.loads(await self.device.readline())
+        if 'error' in response:
+            raise RuntimeError('Error: `%s`' % response['error'])
+        return response['result']
+
+    async def flush(self):
+        while True:
+            data = await self.device.read(1024)
+            if not data:
+                break
+            await asyncio.sleep(.25)
+
+    async def reset(self, delay=8.):
+        '''
+        Example usage:
+
+            future = asyncio.run_coroutine_threadsafe(reset(7.5), adevice.loop)
+            future.add_done_callback(lambda f: print(f.result()))
+        '''
+        try:
+            await self.call('machine.reset')
+        except Exception:
+            pass
+
+        # Wait for device to reboot.
+        print('Wait for device to reboot.')
+        await asyncio.sleep(delay)
+
+        # Flush serial data to reach clean state.
+        print('Flush serial data to reach clean state.')
+        await self.flush()
+        return await self.call('gc.mem_free')
+
+    async def copy_ltor(self, local_path, remote_path, chunk_size=2 * 1024):
+        with open(local_path, 'r') as input_:
+            created = False
+            while True:
+                contents = input_.read(2 * 1024)
+                if not contents:
+                    break
+                if not created:
+                    mode = 'w'
+                    created = True
+                else:
+                    mode = 'a'
+                await self.call('open("%s", "%s").write' % (remote_path, mode), contents)
+                await self.call('gc.collect')
+                print('open("%s", "%s").write %d bytes' % (remote_path, mode, len(contents)))
+
+    async def copy_rtol(self, remote_path, local_path, chunk_size=2 * 1024):
+        parent, name = os.path.split(os.path.realpath(local_path))
+        data = ''
+        while True:
+            contents = await self.call('util.read_at', remote_path, len(data),
+                                       n=chunk_size)
+            await self.call('gc.collect')
+            if not contents:
+                break
+            else:
+                data += contents
+        pathlib.Path(parent).mkdir(parents=True, exist_ok=True)
+        print('write %d bytes to: %s' % (len(data), local_path))
+        with open(local_path, 'w') as output:
+            output.write(data)
